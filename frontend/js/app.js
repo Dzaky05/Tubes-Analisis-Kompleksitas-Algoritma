d43aa2fb076596ebf9e40fbdf0d5534bd5ac45ee
Dzaky <mdzakysyaffatih@gmail.com>
2025-12-27 17:56:07 +0700

    versi saya

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,6 +3,13 @@ const inputN = document.getElementById("inputN");
 const hasilDiv = document.getElementById("hasil");
 const loading = document.getElementById("loading");
 
+const dataCard = document.getElementById("dataCard");
+const dataBox = document.getElementById("dataRandom");
+const btnToggle = document.getElementById("btnToggleData");
+
+let chart = null;
+let dataTerakhir = [];
+
 btn.addEventListener("click", jalankan);
 
 function generateRandomData(n) {
@@ -22,9 +29,11 @@ async function jalankan() {
   }
 
   const data = generateRandomData(n);
+  dataTerakhir = data;
 
   loading.classList.remove("hidden");
   hasilDiv.classList.add("hidden");
+  dataCard.classList.add("hidden");
 
   try {
     const res = await fetch("http://127.0.0.1:5000/api/analisis-modus", {
@@ -40,6 +49,7 @@ async function jalankan() {
     }
 
     tampilkan(json);
+    tampilkanData();
 
   } catch (err) {
     alert("Gagal menghubungi backend:\n" + err.message);
@@ -47,8 +57,6 @@ async function jalankan() {
     loading.classList.add("hidden");
   }
 }
-
-let chart = null;
 
 function tampilkan(res) {
   const it = res.hasil.iteratif.rataRata * 1_000_000;
@@ -83,14 +91,26 @@ function tampilkanGrafik(iteratif, rekursif) {
       datasets: [{
         label: "Waktu Eksekusi (µs)",
         data: [iteratif, rekursif],
-        backgroundColor: ["#43c06d", "#ff6b6b"]
+        backgroundColor: ["#43c06d", "#ff6b6b"] // ganti warna di sini
       }]
     },
     options: {
       responsive: true,
-      scales: {
-        y: { beginAtZero: true }
-      }
+      scales: { y: { beginAtZero: true } }
     }
   });
 }
+
+
+function tampilkanData() {
+  dataCard.classList.remove("hidden");
+  dataBox.textContent = JSON.stringify(dataTerakhir, null, 2);
+}
+
+btnToggle.addEventListener("click", () => {
+  dataBox.classList.toggle("hidden");
+
+  btnToggle.textContent = dataBox.classList.contains("hidden")
+    ? "👁 Tampilkan Data"
+    : "🙈 Sembunyikan Data";
+});
